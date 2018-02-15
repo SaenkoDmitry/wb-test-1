1baa1351dee3cd5bc6ae847e48fdfc15deb28895
SaenkoDmitry <dmitryssaenko@gmail.com>
2018-02-15 23:37:07 +0300

    test done

--- src/cmd/wb-test/main.go
+++ src/cmd/wb-test/main.go
@@ -4,34 +4,81 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"sync"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"strings"
 )
 
 func init() {
 	log.SetFlags(0)
 }
 
-// Other funcs, if any.
+// make http.Get and count appearance of a substring "Go" in resp.Body
+func count(url string) int {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return strings.Count(string(b), "Go")
+}
+
+func worker(urls <-chan string, nums chan<- int, wg *sync.WaitGroup) {
+	for u := range urls {
+		n := count(u)
+		nums <- n
+		fmt.Println("Count for " + u + ":", n)
+		wg.Done()
+	}
+}
 
 func main() {
-
-	// Initialization, if any.
-
 	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanLines)
+
+	urls := make(chan string, 10)
+	nums := make(chan int, 10)
+
+	wg := sync.WaitGroup{}
+
+	// creating five workers
+	for k := 0; k < 5; k++ {
+		go worker(urls, nums, &wg)
+	}
 
 	for scanner.Scan() {
-
-		// Do something with strings here.
-
+		wg.Add(1)
+		urls <- scanner.Text()
 	}
+
 	if err := scanner.Err(); err != nil {
 		log.Fatalln(err)
 	}
 
-	// Other code, if any.
+	// close channel urls to stop range in workers
+	close(urls)
 
 	total := 0
+	wg1 := sync.WaitGroup{}
+	go func(wg *sync.WaitGroup) {
+		wg.Add(1)
+		for r := range nums {
+			total += r
+		}
+		wg.Done()
+	}(&wg1)
 
-	// Other code, if any.
+	// sleep in main goroutine to wait executing all tasks
+	wg.Wait()
+
+	// close channel nums to stop range
+	close(nums)
+	wg1.Wait()
 
 	log.Printf("Total: %v", total)
 }
